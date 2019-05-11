7dbc69934c850315cbee47711f3ec46fcc28054b
缪宇 <miaoyu1@jd.com>
2019-05-11 14:52:41 +0800

    拦截器

--- src/main/java/com/miao/springboot/configFile/MyWebMvcConfiguration.java
+++ src/main/java/com/miao/springboot/configFile/MyWebMvcConfiguration.java
@@ -4,7 +4,7 @@ import com.miao.springboot.interceptor.SomeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-@Configuration
+@Configuration  //当前类为 config类  相当于spring容器
 public class MyWebMvcConfiguration extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
